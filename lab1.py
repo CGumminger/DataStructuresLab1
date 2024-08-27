7c48c40f180c5c48bb541fb46a3fb036858282a6
CGumminger <cgumminger3@gmail.com>
2024-08-26 21:52:12 -0500

    successful left-side multi digit checker

--- lab1.py
+++ lab1.py
@@ -2,6 +2,7 @@
 #Worth 5 points - due friday 8/30 before class. 5 points for a in-person quiz friday about similar tasks.
 
 #1. Write a function reverse_and_uppercase(input_str) that takes a string, reverses it, and converts it to uppercase.
+
 def reverse_and_uppercase(input_str):
     s = input_str[::-1]
     return s.upper()
@@ -10,6 +11,7 @@ assert reverse_and_uppercase("hello") == "OLLEH"
 assert reverse_and_uppercase("DataStructures") == "SERUTCURTSATAD"
 
 #2. Write a function basic_operations(a, b) that returns a tuple containing the sum, difference, product, and quotient of a and b.
+
 def basic_operations(a, b):
     w = a+b
     x = a-b
@@ -20,10 +22,11 @@ def basic_operations(a, b):
 assert basic_operations(10, 2) == (12, 8, 20, 5)
 
 #3. Write a function check_sign(number) that returns "Positive", "Negative", or "Zero" based on the input number.
+
 def check_sign(number):
-    if number>0:
+    if number > 0:
         return "Positive"
-    elif number<0:
+    elif number < 0:
         return "Negative"
     else:
         return "Zero"
@@ -33,17 +36,19 @@ assert check_sign(-3) == "Negative"
 assert check_sign(0) == "Zero"
 
 #4. Write a function sum_of_evens(numbers) that returns the sum of all even numbers in a list.
+
 def sum_of_evens(numbers):
     total = 0
     for x in numbers:
-        if x%2 == 0:
-            total +=x
+        if x % 2 == 0:
+            total += x
     return total
 
 assert sum_of_evens([1, 2, 3, 4, 5, 6]) == 12
 assert sum_of_evens([7, 8, 9, 10]) == 18
 
 #5. Write a recursive function fibonacci(n) that returns the nth Fibonacci number. Without recursion. Must use loop.
+
 def fibonacci(n):
     if n == 0:
         return 0
@@ -93,11 +98,22 @@ def parse_and_calculate(expression):
             multiplyAndDivide = False
     while addAndSubtract == True:
         if ("+" in expression or "-" in expression):
-            index=0
+            index = 0
+            left = 1
+            right = 1
             for c in expression:
                 if c=="+":
-                    s = add(int(expression[index-1]), int(expression[index+1]))
-                    expression = expression[:index-1] + s + expression[index+2:]
+                    while fullNumLeft==False:
+                        if index-left == 0:
+                            fullNumLeft = True
+                        elif expression[index-left].isdigit():
+                            left += 1
+                        else:
+                            if left!=1: 
+                                left -= 1
+                            fullNumLeft = True
+                    s = add(int(expression[index-left:index]), int(expression[index+1]))
+                    expression = expression[:index-left] + s + expression[index+2:]
                     break
                 elif c=="-":
                     s = subtract(int(expression[index-1]), int(expression[index+1]))
